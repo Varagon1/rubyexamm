d25d2258ba31d5b164d36c70c1ce3f3ca9a4b5cd
Victoria Aragon <Victoria.Aragon415@gmail.com>
2014-03-26 20:09:56 +0000

    first program

---
+++ bulls2.rb
@@ -0,0 +1,9 @@
+puts "Do you like the bulls?"
+answer = gets.chomp
+if answer == "yes"
+  puts "Go Bulls!"
+else
+  if answer == "no"
+    puts "Come back when you are a real baketball fan!"
+  end
+end
